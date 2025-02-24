e707323b8f019646897f1db5779bfecbf52e6962
Sagnik Majumder <sagnikm183@gmail.com>
2025-02-24 21:35:51 +0530

    fixed login

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Navbar from './components/ui/Navbar/Navbar'
 import Login from './pages/auth/Login'
+import HomePage from './pages/home/HomePage'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className='h-full'>
         <Navbar />
         <Routes>
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </div>
