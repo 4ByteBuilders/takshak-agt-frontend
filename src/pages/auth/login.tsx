594be69e559f94a7314f77ccd6a5d5142a068638
Sagnik Majumder <sagnikm183@gmail.com>
2025-02-24 23:13:13 +0530

    added 404 and added checkAuth status in navbar

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,21 +1,29 @@
 import { supabase } from "@/supabaseClient";
 
+
 const Login = () => {
+
   const handleGoogleLogin = async () => {
-    const { data: { user } } = await supabase.auth.getUser()
+    const { data: { user } } = await supabase.auth.getUser();
     if (user) {
-      console.log('User already logged in')
-      console.log(user)
+      console.log('User already logged in');
+      console.log(user);
     } else {
-      const { data, error } = await supabase.auth.signInWithOAuth({
+      const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
+        options: {
+          redirectTo: `${window.location.origin}/home`,
+        },
       });
       if (error) console.error('Error logging in with Google:', error.message);
-      else {
-        console.log(data)
-      }
+
     }
   };
+
+  const handleLogout = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) console.error('Error logging out:', error.message);
+  }
 
   return (
     <div className="flex items-center justify-center text-center h-full p-3">
@@ -32,6 +40,12 @@ const Login = () => {
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
         >
           Sign in with Google
+        </button>
+        <button
+          onClick={handleLogout}
+          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+        >
+          Logout
         </button>
       </div>
     </div>
--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,7 +1,11 @@
 
 function HomePage() {
     return (
-        <div>HomePage</div>
+        <div className="flex flex-col items-center justify-center text-center h-full p-5 text-4xl font-extrabold">
+            <p className="mb-7 max-w-lg">
+                Events
+            </p>
+        </div>
     )
 }
 
