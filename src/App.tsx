98fefa929bf82b9fc24355745abc62647ac7d46a
Pratik <sarangipratikcode@gmail.com>
2025-02-24 23:43:21 +0530

    authentication rerouting fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/ui/Navbar/Navbar'
-import Login from './pages/auth/login'
-import HomePage from './pages/home/HomePage'
-import Page404 from './pages/404/404'
-import EventPage from './pages/EventPages/EventPage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/ui/Navbar/Navbar";
+import Login from "./pages/auth/login";
+import HomePage from "./pages/home/HomePage";
+import Page404 from "./pages/404/404";
+import EventPage from "./pages/EventPages/EventPage";
 function App() {
   return (
     <BrowserRouter>
@@ -12,7 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<EventPage />} />
           <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Page404 />} /> {/* Redirect to home if no matching route found */}
+          <Route path="*" element={<Page404 />} />{" "}
+          {/* Redirect to home if no matching route found */}
         </Routes>
       </div>
     </BrowserRouter>
