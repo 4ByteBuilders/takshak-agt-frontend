70f0ab55222cf5318790ee80afe729e76f439634
BHAVYAJAIX <bhavyajain16104@gmail.com>
2025-02-24 23:24:52 +0530

    temp

--- src/components/ui/Navbar/Navbar.tsx
+++ src/components/ui/Navbar/Navbar.tsx
@@ -1,10 +1,9 @@
-
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Button } from "../button"
 
 function Navbar() {
     return (
-        <div className="fixed top-0 left-0 w-full shadow-sm shadow-slate-700">
+        <div className="top-0 left-0 w-full shadow-sm shadow-slate-700">
             <div className="flex items-center p-2 justify-between">
                 <div className="flex gap-4 items-center font-alfa text-2xl">
                     <img src="/takshak-logo.png" className="max-h-10" />
