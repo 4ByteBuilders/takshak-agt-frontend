0f45b909c03c697f8558a14d9799a89065c864f8
Sagnik Majumder <sagnikm183@gmail.com>
2025-02-25 12:58:29 +0530

    added Auth context and logout button

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navbar from "./components/ui/Navbar/Navbar";
-import Login from "./pages/auth/login";
-import HomePage from "./pages/home/HomePage";
-import Page404 from "./pages/404/404";
-import EventPage from "./pages/EventPages/EventPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Navbar from './components/ui/Navbar/Navbar'
+import Login from './pages/auth/Login'
+import HomePage from './pages/home/HomePage'
+import Page404 from './pages/404/404'
+import EventPage from './pages/EventPages/EventPage';
 function App() {
   return (
     <BrowserRouter>
       <div className="h-full">
         <Navbar />
         <Routes>
-          <Route path="/" element={<EventPage />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Page404 />} />{" "}
-          {/* Redirect to home if no matching route found */}
+          <Route path="/events" element={<EventPage />} />
+          <Route path="*" element={<Page404 />} />
         </Routes>
       </div>
     </BrowserRouter>
