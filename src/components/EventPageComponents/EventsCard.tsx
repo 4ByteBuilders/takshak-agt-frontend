56e69b77a6e40f31882058e211bd3411b9f3a7ae
BHAVYAJAIX <bhavyajain16104@gmail.com>
2025-02-26 17:53:23 +0530

    prices and their type fixed

--- src/components/EventPageComponents/EventsCard.tsx
+++ src/components/EventPageComponents/EventsCard.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { MapPin, Calendar, Armchair } from 'lucide-react';
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card";
 
 import { Button } from "@/components/ui/button";
@@ -27,13 +28,13 @@ interface Event {
 
 const EventCard = ({ name, place, date, availability, description, price, image }: Event) => {
   return (
-    <Card className="flex flex-col h-full rounded-lg shadow-md transition duration-300 ease-in-out hover:scale-110">
-      <div className="flex justify-center">
+    <Card className="flex flex-col h-full rounded-lg transition duration-300 ease-in-out hover:border-stone-50">
+      <div className="flex justify-center items-center">
         <img src={image[0]} alt={name} className="rounded-t-lg w-full h-40 object-cover" />
       </div>
       <CardHeader>
         <CardTitle className="text-xl font-bold">{name}</CardTitle>
-        <div className="flex justify-between mt-2">
+        <div className="flex-row sm:flex justify-between mt-2">
           <div>
             <div className="flex items-center mt-2">
               <MapPin strokeWidth={"1px"} size={"16px"} className="mr-2" />
@@ -47,28 +48,28 @@ const EventCard = ({ name, place, date, availability, description, price, image 
               <Armchair strokeWidth={"1px"} size={"16px"} className="mr-2" />
               <CardDescription className="inline-block">{availability.available} / {availability.total} available</CardDescription>
             </div>
-            <ul className="flex flex-wrap w-full items-center justify-center gap-4 mt-4">
-              {price.map((priceDetail, idx) => (
-                <li
-                  key={idx}
-                  className="bg-green-500/20 backdrop-blur-md border border-green-400/50 shadow-xl rounded-xl px-3 py-2 text-sm font-semibold text-white drop-shadow-[0_0_10px_rgba(34,197,94,0.8)]"
-                >
-                  <span className="block text-green-300">{priceDetail.type}</span>
-                  <span className="text-xs font-bold">₹ {priceDetail.price}</span>
-                </li>
-              ))}
-            </ul>
-
           </div>
+          <ul className="flex flex-row items-end sm:flex-col gap-4 mt-4">
+            {price.map((priceDetail, idx) => (
+              <li
+                key={idx}
+                className="bg-green-500/20 backdrop-blur-md border border-green-400/50 shadow-xl rounded-xl px-2 py-1 text-sm font-semibold text-white drop-shadow-[0_0_10px_rgba(34,197,94,0.8)] w-fit"
+              >
+                <span className="text-green-300">{priceDetail.type+"  "}</span>
+                <span className="text-xs font-bold">₹ {priceDetail.price}</span>
+
+              </li>
+            ))}
+          </ul>
         </div>
       </CardHeader>
-      <CardContent className='min-h-100'>
+      <CardContent className='min-h-[100px]'>
         <CardDescription>
-          {description}
+          <p className="mt-2">{description}</p>
         </CardDescription>
       </CardContent>
-      <CardContent className='min-h-100'>
-        <Button className="w-full hover:shadow-2xl hover:drop-shadow-[0_0_8px_rgba(34,197,94,0.8)]">Book Now</Button>
+      <CardContent>
+        <Button className="w-full bg-green-50 transition duration-300 ease-in-out hover:shadow-2xl hover:drop-shadow-[0_0_8px_rgba(34,197,94,0.8)]">Book Now</Button>
       </CardContent>
     </Card>
   );
@@ -80,7 +81,7 @@ interface EventsCardProps {
 
 const EventsCard = ({ events }: EventsCardProps) => {
   return (
-    <div className="m-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+    <div className="m-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 lg:mx-12">
       {events.map((event, index) => (
         <EventCard
           key={index}
