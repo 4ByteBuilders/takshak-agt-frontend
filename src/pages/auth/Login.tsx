9309a23e3d2c8b5f791dfd660a6f33549d385466
BHAVYAJAIX <bhavyajain16104@gmail.com>
2025-02-28 01:47:04 +0530

    I created an awesome login page

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -5,45 +5,47 @@ import { toast } from "sonner"
 
 const Login = () => {
   const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   const checkUser = async () => {
-  //     const {
-  //       data: { user },
-  //     } = await supabase.auth.getUser();
-  //     if (user) {
-  //       navigate("/");
-  //     }
-  //   };
-  //   checkUser();
-  // }, [navigate]);
+  useEffect(() => {
+    const checkUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      if (user) {
+        navigate("/");
+      }
+    };
+    checkUser();
+  }, [navigate]);
 
   const handleGoogleLogin = async () => {
-    const { data, error } = await supabase.auth.signInWithOAuth({
+    const { error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        // redirectTo: `${window.location.origin}/`,
+        redirectTo: `${window.location.origin}/`,
       },
     });
-    console.log(data);
     if (error) toast.error(error.message);
   };
 
   return (
-    <div className="flex items-center justify-center text-center h-full p-3">
-      <div>
-        <p className="text-4xl font-bold mb-2">স্বাগতম!</p>
-        <p className="text-md font-thin">
-          Let your event journey begin with the festive vibe of Agartala
+    <div className="flex flex-col md:flex-row items-center text-center h-full p-3">
+      <div className="flex flex-col w-1/2 items-start ml-10 ">
+        <p className="text-6xl font-black my-5">স্বাগতম!</p>
+        <p className="text-4xl font-bold my-5">We are <span className="text-secondary">Takshak</span> Events</p>
+
+        <p className="text-md font-normal my-5 w-3/4 text-left">
+          Let your event journey begin with the festive vibe of Agartala! Explore the best events around Agartala with the best deals!
         </p>
-        <div className="mt-2 border" />
+        <div className="border-b-2 border-gray-300" />
         <button
           onClick={handleGoogleLogin}
-          className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+          className="mt-4 px-4 py-2 bg-primary text-neutral-900 font-semibold rounded-md"
         >
           Sign in with Google
         </button>
-        <div className=""></div>
+      </div>
+      <div className="md:w-1/2 p-4">
+        <img src="/LoginLogo.png" alt="Login Logo" className="w-full h-auto" />
       </div>
     </div>
   );
