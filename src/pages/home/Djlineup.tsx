b17d88f1f282073a616cd101e62b4f2325b5bfdd
Sagnik Majumder <sagnikm183@gmail.com>
2025-03-08 21:10:38 +0530

    fixed build

--- src/pages/home/Djlineup.tsx
+++ src/pages/home/Djlineup.tsx
@@ -34,7 +34,7 @@ function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
     const x = useTransform(baseX, (v) => `${wrap(-50, 0, v)}%`);
 
     const directionFactor = useRef<number>(1);
-    useAnimationFrame((t, delta) => {
+    useAnimationFrame((_, delta) => {
         let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
 
         if (velocityFactor.get() < 0) {
